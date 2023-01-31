b5541b5f025f259b8727544604d549a7d2bcd6f1
Carlos Villegas <carlos.villegasch@gmail.com>
2023-01-31 13:58:39 -0500

    App that show us static resources in the public folder using a middleware named express.static

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const app = express();
 //Importamos nuestro motor de plantillas EJS
 app.set('view engine', 'ejs');
 
+//Usar Middleware express static para mostrar lo que hay en la carpeta PUBLIC
+app.use(express.static('public')); //Dentro de las comillas ponemos el nombre de la carpeta que contiene los elementos estáticos
+
+//Usar directorios estáticos con "nombre de la carpeta" y la palabra "__dirname" para que los recursos funcionen siempre en cualquier lugar
+app.use('/recursos', express.static(__dirname+'/public'));
+console.log(__dirname);
+
+//Obtenemos la plantilla EJS
+app.get('/', (req, res)=>{
+    res.render('index.ejs'); //Podemos quitarle la extensión si queremos
+});
+
 app.listen(3000, (req, res)=>{
     console.log("SERVIDOR RUNNING");
 });
